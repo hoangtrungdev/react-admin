fda0c3be7cfc4b9268b407a9689e7702fbef4d78
trung.hoang <trung.hoang@chudu24.com>
2019-01-03 10:09:57 +0700

    update code

--- src/firebaseModule.js
+++ src/firebaseModule.js
@@ -1,14 +1,8 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
 
-const config = {
-  apiKey: "AIzaSyDGUYSLmWfXhgV5xS1wXx9oc--REhbRJfg",
-  authDomain: "halley-hoangtrungdev.firebaseapp.com",
-  databaseURL: "https://halley-hoangtrungdev.firebaseio.com",
-  projectId: "halley-hoangtrungdev",
-  storageBucket: "halley-hoangtrungdev.appspot.com",
-  messagingSenderId: "716358595273"
-};
+import { config } from "../FirebaseConfig";
+
 firebase.initializeApp(config);
 
 export default firebase;
